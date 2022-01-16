ff07d72756aac7ef962d34ee80486cf426521e2c
tastupen <tatsupen0828@au.com>
2022-01-16 23:14:27 +0000

    deploy 11

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -9,6 +9,8 @@ class SearchsController < ApplicationController
         @products += Product.where(["name like ? OR company like ?", "#{keyword}%", "#{keyword}%"]).where(price: @price)
         @genres = Product.joins(:genre).where(["genres.name like ? OR genres.major_category_name like ?", "#{keyword}%", "#{keyword}%"]).where(price: @price)
       end
+    elsif params[:price].present?
+      @products += Product.where(price: Float::MIN..params[:price])
     else
       @keywords.each do |keyword|
         @products += Product.where(["name like ? OR company like ?", "#{keyword}%", "#{keyword}%"])
